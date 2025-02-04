1d7251dc81ad3c509013a9dc7eaaf9b1f6401d8e
hyunki <hk7286@naver.com>
2025-02-04 17:54:19 +0900

    update: 테마 기능 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import {Card} from "@mui/material";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>hi</h1>
-    </div>
-  );
+    return (
+        <Container maxWidth="lg">
+            <Box
+                sx={{
+                    my: 4,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+                    Material UI - Next.js App Router example in TypeScript
+                </Typography>
+                <Card>
+                    Next.js
+                </Card>
+                <Card>
+                    MUI
+                </Card>
+            </Box>
+        </Container>
+    );
 }
